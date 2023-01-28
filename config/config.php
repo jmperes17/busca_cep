1846119b5056796d27285b2f400da17767145e58
unknown <jmperes17@gmail.com>
2023-01-28 13:48:29 -0300

    refatorando código

--- config/config.php
+++ config/config.php
@@ -1,9 +1,11 @@
 <?php
 
+session_start();
+
 if (isset($_POST['cep_input'])) {
     $cep = preg_replace("/[^0-9]/", "", $_POST['cep_input']);
 } else {
-    $cep = "21775530";
+    header('Location: index.php');
 }
 
 $url = "https://viacep.com.br/ws/$cep/json/";
@@ -17,15 +19,16 @@ $response = curl_exec($ch);
 
 curl_close($ch);
 
-//Transformando em array
+// Transformando em array
 $data = json_decode($response);
 
-if (!empty($data)){
-    header('Location: index.php');
-}
+$_SESSION["data"] = $data;
 
-/*
-foreach($data as $value => $key){
-    echo $value ."-> ". $key . "</br>";
-}*/
-?>
+if (!empty($data)) {
+    header('Location: ../index.php');
+} else {
+    echo '<script type="text/javascript">';
+    echo 'alert("CEP NÃO ENCONTRADO OU INVÁLIDO");';
+    echo 'window.location.href = "../index.php";';
+    echo '</script>';
+}
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php session_start();
+
+if (isset($_SESSION["data"])) {
+    $cep = $_SESSION["data"]->cep;
+    $data = $_SESSION["data"];
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,42 +14,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />
     <title>API - VIA CEP</title>
 </head>
-
 <body>
-    <div class="col-md-10">
-        <form action="config/config.php" method="post">
-            <label for="cep">CEP</label>
-            <input type="text" name="cep_input" placeholder="Informe o CEP" minlength="8">
-            <button type="submit" class="btn btn-success">Pesquisar Endereço</button>
-        </form>
+    <div class="container" id="container">
+        <div class="box" id="box">
+            <form action="config/config.php" method="post">
+                <label for="cep">CEP</label>
+                <input type="text" name="cep_input" placeholder="Informe o CEP" minlength="8" maxlength="9" required>
+                <button type="submit" class="btn btn-success">Buscar Endereço</button>
+            </form>
+            <?php if (isset($cep)) : ?>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">CEP</th>
+                            <th scope="col">Logradouro</th>
+                            <th scope="col">Bairro</th>
+                            <th scope="col">Localidade</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?= $data->cep; ?></td>
+                            <td><?= $data->logradouro ?></td>
+                            <td><?= $data->bairro ?></td>
+                            <td><?= $data->localidade ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            <?php endif; ?>
+        </div>
     </div>
-    <?php require_once "config/config.php" ?>
-
-    <div class="col-md-8">
-        <?php if (isset($cep)) { ?>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">CEP</th>
-                        <th scope="col">Logradouro</th>
-                        <th scope="col">Bairro</th>
-                        <th scope="col">Localidade</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><?= $data->cep; ?></td>
-                        <td><?= $data->logradouro ?></td>
-                        <td><?= $data->bairro ?></td>
-                        <td><?= $data->localidade ?></td>
-                    </tr>
-                </tbody>
-            </table>
-        <?php } ?>
-    </div>
-
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
